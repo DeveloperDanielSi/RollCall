91fc1127b53dd9161621fbdd38ce561e25224eec
Daniel Simard <daniel.simard@uri.edu>
2024-06-08 12:39:55 -0400

    implemented additional pages and attempted logic

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using System.Net.Http;
 using RollCall;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,18 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.Authority = "https://accounts.google.com";
+    options.ProviderOptions.ClientId = "968520920745-bghd5ldv30hv1ofvuh4gqm201s7p95sj.apps.googleusercontent.com";
+    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.RedirectUri = builder.HostEnvironment.BaseAddress + "authentication/login-callback";
+    options.ProviderOptions.PostLogoutRedirectUri = builder.HostEnvironment.BaseAddress;
+
+    options.ProviderOptions.DefaultScopes.Add("openid");
+    options.ProviderOptions.DefaultScopes.Add("profile");
+    options.ProviderOptions.DefaultScopes.Add("email");
+});
 
 await builder.Build().RunAsync();
