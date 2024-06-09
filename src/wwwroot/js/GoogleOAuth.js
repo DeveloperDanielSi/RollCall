b4345d7eba08cb0e9ad34b4a61d85a34ea2f5a51
TheGreatestDaniel <daniel.simard@uri.edu>
2024-06-08 21:51:41 -0400

    update

---
+++ src/wwwroot/js/GoogleOAuth.js
@@ -0,0 +1,19 @@
+﻿let blazorSchoolAuthenticationStateProviderInstance = null;
+
+function blazorSchoolGoogleInitialize(clientId, blazorSchoolAuthenticationStateProvider) {
+    blazorSchoolAuthenticationStateProviderInstance = blazorSchoolAuthenticationStateProvider;
+    google.accounts.id.initialize({ client_id: clientId, callback: blazorSchoolCallback });
+}
+
+function blazorSchoolGooglePrompt() {
+    google.accounts.id.prompt((notification) => {
+        if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
+            console.info(notification.getNotDisplayedReason());
+            console.info(notification.getSkippedReason());
+        }
+    });
+}
+
+function blazorSchoolCallback(googleResponse) {
+    blazorSchoolAuthenticationStateProviderInstance.invokeMethodAsync("GoogleLogin", { ClientId: googleResponse.clientId, SelectedBy: googleResponse.select_by, Credential: googleResponse.credential });
+}
