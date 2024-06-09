a4a7b480994332039434a616b804575c72c85156
TheGreatestDaniel <daniel.simard@uri.edu>
2024-06-09 15:22:57 -0400

    google authentication fix

--- src/Program.cs
+++ src/Program.cs
@@ -10,10 +10,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// Configure OIDC Authentication with Google
 builder.Services.AddOidcAuthentication(options =>
 {
     options.ProviderOptions.Authority = "https://accounts.google.com";
-    options.ProviderOptions.ClientId = "968520920745-bghd5ldv30hv1ofvuh4gqm201s7p95sj.apps.googleusercontent.com";
+    options.ProviderOptions.ClientId = "968520920745-bghd5ldv30hv1ofvuh4gqm201s7p95sj.apps.googleusercontent.com"; // Use your Google Client ID
     options.ProviderOptions.ResponseType = "code";
     options.ProviderOptions.RedirectUri = builder.HostEnvironment.BaseAddress + "authentication/login-callback";
     options.ProviderOptions.PostLogoutRedirectUri = builder.HostEnvironment.BaseAddress;
@@ -21,6 +22,9 @@ builder.Services.AddOidcAuthentication(options =>
     options.ProviderOptions.DefaultScopes.Add("openid");
     options.ProviderOptions.DefaultScopes.Add("profile");
     options.ProviderOptions.DefaultScopes.Add("email");
+
+    // Use PKCE by setting the ResponseMode to 'query'
+    options.ProviderOptions.ResponseMode = "query";
 });
 
 await builder.Build().RunAsync();
